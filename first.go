35208e4af5be25a1310b8782e4b4b6c45edd02d3
Mahalakshmitalluri <96261349+Mahalakshmitalluri@users.noreply.github.com>
2021-12-29 17:40:45 -0600

    Delete first.go

--- first.go
+++
@@ -1,9 +0,0 @@
-package main
-import  "github.com/Mahalakshmitalluri/assignments/variable" // The custom package which we created is imported here as same as we import inbuilt package in files.
-
-func main(){   // main function is used to  process and execute the code.
-	variable.Add()       /* package variable  is imported and function add,sub, mul,div is imported  */
-	variable.Subtract()   
-	variable.Multiple()
-	variable.Div()
-}
